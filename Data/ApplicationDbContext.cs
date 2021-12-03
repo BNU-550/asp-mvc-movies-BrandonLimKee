72ba366df9d514663e8da7ccfb639b9169131be3
BrandonLimKee <78452249+BrandonLimKee@users.noreply.github.com>
2021-12-03 02:55:31 +0000

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MVC_Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVC_Movies.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Movies.Models
+{
+    public enum Genres
+    {
+        Comedy, Drama, Horror, Action
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Movie
+    {
+        [Key]
+        public int ID { get; set; }
+        [StringLength(60), Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public Genres Genre { get; set; }
+        [Range(5.99, 7.99), Required]
+        public decimal Price { get; set; }
+    }
+}
